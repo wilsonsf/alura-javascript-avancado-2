53eeeeb0266cba12d0f092a3aa136b31b7550f89
Wilson Farias <wilsonsf1@gmail.com>
2017-03-27 08:08:31 -0300

    Usando Arrow Function para substituir passagem de contexto.
    Arrow functions possuem o contexto léxico, mantendo o contexto no momento da criação do código, enquanto o this passado por função tem seu contexto dinâmico, mudando de acordo com o local onde é chamado.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,12 +6,8 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes(this, function(model){
-            this._negociacoesView.update(model);
-            // Apesar de ser declarado antes da inicialização, só será
-            // executado posteriormente.
-        });
-        
+        this._listaNegociacoes = new ListaNegociacoes((model) => this._negociacoesView.update(model));
+            
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         
--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,17 +1,14 @@
 class ListaNegociacoes {
     
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
-    }
+	}  
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-        // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
     
     get negociacoes() {
@@ -21,7 +18,5 @@ class ListaNegociacoes {
 
     esvazia() {
     	this._negociacoes = [];
-    	// this._armadilha(this);
-    	Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
