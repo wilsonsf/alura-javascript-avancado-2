144080d7f348d607ff6113c8b744d99d2c5a230c
Wilson Farias <wilsonsf1@gmail.com>
2017-03-27 08:08:31 -0300

    Usando Arrow Function para substituir passagem de contexto.
    Arrow functions possuem o contexto léxico, mantendo o contexto no momento da criação do código, enquanto o this passado por função tem seu contexto dinâmico, mudando de acordo com o local onde é chamado.

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,17 +1,16 @@
 class ListaNegociacoes {
     
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
-    }
+	}  
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
         // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
     
     get negociacoes() {
@@ -22,6 +21,6 @@ class ListaNegociacoes {
     esvazia() {
     	this._negociacoes = [];
     	// this._armadilha(this);
-    	Reflect.apply(this._armadilha, this._contexto, [this]);
+    	// Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
