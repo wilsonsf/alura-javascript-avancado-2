79b97369987fbb17e852f10e08a43cea536f9c0e
Wilson Farias <wilsonsf1@gmail.com>
2017-04-20 17:59:13 -0300

    Fix: método ListaNegociacoes.adiciona estava incompleto.
    Removendo trailing whitespaces
    Adaptando Proxy para ListaNegociacoes - não funciona pois adiciona não usa setters em sua operação.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,7 @@
 class NegociacaoController {
-    
+
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
@@ -9,26 +9,26 @@ class NegociacaoController {
 
         /*
         this._listaNegociacoes = new ListaNegociacoes((model) => this._negociacoesView.update(model));
-          */  
+          */
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
-        
+
     }
-    
+
     adiciona(event) {
-        
+
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
-        
-        this._limpaFormulario();   
+
+        this._limpaFormulario();
     }
 
     apaga() {
@@ -38,20 +38,20 @@ class NegociacaoController {
         this._mensagem.text = 'Negociações apagadas com sucesso.';
         this._mensagemView.update(this._mensagem);
     }
-    
+
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value);    
+            this._inputValor.value);
     }
-    
+
     _limpaFormulario() {
-     
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();   
+        this._inputData.focus();
     }
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,13 @@
 class ListaNegociacoes {
-    
+
     constructor() {
         this._negociacoes = [];
-	}  
-    
-    adiciona() {
+	}
+
+    adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
-    
+
     get negociacoes() {
         return [].concat(this._negociacoes);
     }
